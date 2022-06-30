40426b3263e269d9558314f40c8d92126c3aea5a
lemys lopez <lemyskaman@gmail.com>
2022-06-30 16:25:42 -0400

    format

--- app/Space/helpers.php
+++ app/Space/helpers.php
@@ -144,7 +144,7 @@ function format_money_pdf($money, $currency = null)
     if ($currency->swap_currency_symbol) {
         $currency_with_symbol = $format_money.'<span style="font-family: DejaVu Sans;"> '.$currency->symbol.'</span>';
     } else {
-        $currency_with_symbol = '<span style="font-family: DejaVu Sans;"> '.$currency->symbol.'</span>'.$format_money;
+        $currency_with_symbol = '<span style="font-family: DejaVu Sans;"> '.$currency->symbol.' </span>'.$format_money;
     }
 
     return $currency_with_symbol;
