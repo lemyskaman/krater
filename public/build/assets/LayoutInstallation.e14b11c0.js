562b772c021d8cceccedcde65f9f1ae6902713be
lemyskaman <lemyskaman@gmail.com>
2022-08-02 18:20:27 -0400

    front build

---
+++ public/build/assets/LayoutInstallation.e14b11c0.js
@@ -0,0 +1,1 @@
+import{N as t}from"./NotificationRoot.53cc6e6c.js";import{r as s,o as r,e as a,f as e,h as c}from"./vendor.2f2e72ef.js";import"./main.d9e9868f.js";const n={class:"h-screen overflow-y-auto text-base"},i={class:"container mx-auto px-4"},u={setup(_){return(f,m)=>{const o=s("router-view");return r(),a("div",n,[e(t),c("div",i,[e(o)])])}}};export{u as default};
